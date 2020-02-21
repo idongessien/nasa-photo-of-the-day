bcf8a0cb339d2e4c9d9cb6c9435cb6140b8aa6e2
Idong Essien <idong.essien@comcast.net>
2020-02-20 20:28:28 -0500

    fixed image render issue

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+const ImageContainer = styled.div`
+ background: rgb(245, 245, 245);
+ padding: 2%;
+ width: 70%;
+ margin: 0 auto;
+ font-size: 1.5rem;
+ line-height: 2.5rem;
+`
+const HdImg = styled.img`
+   width:100%;
+   height: 50vh;
+`
 
 const ImageCard = props => {
    return (
-      <div className="img-card" key="">
+      <ImageContainer>
          <h2>{props.title}</h2>
-         <img src={props.hdurl} alt="random generated from NASA API" />
+         <HdImg src={props.image} alt="random generated from NASA API" />
          <p>{props.date}</p>
          <p>{props.explanation}</p>
-      </div>
+      </ImageContainer>
    );
 };
 export default ImageCard;
--- src/components/ImageComponents.jsx
+++ src/components/ImageComponents.jsx
@@ -11,6 +11,7 @@ function ImageComponents() {
             "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-02-19"
          )
          .then(response => {
+            console.log(response.data);
             setInfos(response.data);
          })
          .catch(error => {
